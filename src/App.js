7c9ebc707737ec502fb20baa4981f466164d530e
Jesse Taavola <46852381+jtaavola@users.noreply.github.com>
2022-08-09 21:36:35 -0500

    add box breathing animation (#1)

--- src/App.js
+++ src/App.js
@@ -1,58 +1,18 @@
-import logo from './logo.svg';
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
+import BoxBreathingGuide from './components/BoxBreathingGuide';
 
 const AppContainer = styled.div`
-  text-align: center;
-`;
-
-const AppHeader = styled.header`
-  background-color: #282c34;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
   align-items: center;
   justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
-`;
-
-const AppLogoSpin = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`;
-
-const AppLogo = styled.img`
-  height: 40vmin;
-  pointer-events: none;
-  @media (prefers-reduced-motion: no-preference) {
-    animation: ${AppLogoSpin} infinite 20s linear;
-  }
-`;
-
-const AppLink = styled.a`
-  color: #61dafb;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
 `;
 
 function App() {
   return (
     <AppContainer>
-      <AppHeader>
-        <AppLogo src={logo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <AppLink
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </AppLink>
-      </AppHeader>
+      <BoxBreathingGuide />
     </AppContainer>
   );
 }
