befa18245a4b792f91e3df6de714d32f4019f657
jtaavola <jessetaavola@gmail.com>
2022-09-30 19:18:16 -0500

    add github repo link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import BoxBreathingGuide from './components/BoxBreathingGuide';
+import { SiGithub } from 'react-icons/si';
 
 const AppContainer = styled.div`
   align-items: center;
@@ -9,9 +10,18 @@ const AppContainer = styled.div`
   min-height: 100vh;
 `;
 
+const RepoLink = styled.a`
+  position: fixed;
+  top: 0.5rem;
+  right: 0.5rem;
+`;
+
 function App() {
   return (
     <AppContainer>
+      <RepoLink href="https://github.com/jtaavola/breathe" target="_blank">
+        <SiGithub size="1.5rem" title="Repo link" />
+      </RepoLink>
       <BoxBreathingGuide />
     </AppContainer>
   );
